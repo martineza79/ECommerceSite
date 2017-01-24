3e13a7c5af13b6b9aabef9e094557c887e356fee
Alex Martinez <martineza79@outlook.com>
2017-01-24 12:53:53 -0600

    Update EmailSettings.cs

--- ECommerceSite.Domain/Concrete/EmailSettings.cs
+++ ECommerceSite.Domain/Concrete/EmailSettings.cs
@@ -8,12 +8,12 @@ namespace ECommerceSite.Domain.Concrete
 {
     public class EmailSettings
     {
-        public string MailToAddress = "martineza79@outlook.com"; //use your own e-mail address with in the double quotation marks
-        public string MailFromAddress = "martineza79@outlook.com"; //use your own e-mail address with in the double quotation marks
+        public string MailToAddress = ""; //use your own e-mail address with in the double quotation marks
+        public string MailFromAddress = ""; //use your own e-mail address with in the double quotation marks
         public bool UseSsl = true;
-        public string Username = "martineza79@outlook.com"; //use your own e-mail address with in the double quotation marks
-        public string Password = "ivonne11";//use your own password
-        public string ServerName = "smtp.live.com"; //use your own server name i.e. "smtp.gmail.com"
+        public string Username = ""; //use your own e-mail address with in the double quotation marks
+        public string Password = "";//use your own password
+        public string ServerName = ""; //use your own server name i.e. "smtp.gmail.com"
         public int ServerPort = 587; //use your own e-mail server port number (remember, server port number is an int i.e. 587).
     }
 }
